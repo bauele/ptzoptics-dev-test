8c290678ec291ffe99ca7871234691b504941264
bauele <bauele@users.noreply.github.com>
2025-01-07 13:02:01 -0500

    Commit work to pull on remote server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-let message: string;
-message = "Hello, world!";
+import { Server } from "socket.io";
 
-console.log(message);
+const io = new Server(3000, {
+  /* options */
+});
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+const ipAddress = "ip";
+const port = "port";
+
+(async function sendServerInfo() {
+  let response = await fetch("http://localhost:8080", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ipAddress: ipAddress, port: port }),
+  });
+  let data = response.status;
+  console.log(data);
+})();
