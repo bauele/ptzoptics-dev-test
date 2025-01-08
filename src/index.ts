8ffc48c9a76132172e129e4330addb8fc94f0cf3
bauele <bauele@users.noreply.github.com>
2025-01-08 10:10:06 -0500

    Create local TCP server and send remote server its ip address and port

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,66 @@
-import { Server } from "socket.io";
+require("dotenv").config();
+const net = require("net");
 
-const io = new Server(3000, {
-  /* options */
+//  Read in the provided IP address and port
+//  If this cannot be found, inform the user
+(function getConfigurationValues() {
+  if (
+    !process.env.IP_ADDRESS ||
+    !process.env.PORT ||
+    !process.env.TEST_ADDRESS
+  ) {
+    console.log(
+      "Error: Could not obtain ip address and/or port from .env file. Please refer to example.env for more information."
+    );
+
+    //  Exit with code 1
+    process.exit(1);
+  }
+
+  //  Print out obtained configuration values
+  console.log(
+    `Obtained following configuration values: \nServer IP: ${process.env.IP_ADDRESS} \nServer Port: ${process.env.PORT}\nTest Address: ${process.env.TEST_ADDRESS}`
+  );
+})();
+
+//  Create the TCP server
+const server = net.createServer((socket: any) => {
+  console.log("Recieved connection");
+
+  //  Handle data received from the client
+  socket.on("data", (data: any) => {
+    console.log("Recieved data");
+    socket.write("Thank you for connecting!");
+  });
+
+  //  Handle client disconnection
+  socket.on("end", () => {
+    console.log("Client disconnected");
+  });
+
+  //  Handle errors
+  socket.on("error", (err: any) => {
+    console.error("An error occured: ", err);
+  });
 });
 
-io.on("connection", (socket) => {
-  // ...
+//  Start the TCP server
+server.listen(process.env.PORT, () => {
+  console.log(`TCP server is listening on port ${process.env.PORT}`);
 });
 
-const ipAddress = "ip";
-const port = "port";
-
+//  Send IP address and port to remote server
 (async function sendServerInfo() {
-  let response = await fetch("http://localhost:8080", {
+  let response = await fetch(`${process.env.TEST_ADDRESS}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ ipAddress: ipAddress, port: port }),
+    body: JSON.stringify({
+      ipAddress: `${process.env.IP_ADDRESS}`,
+      port: Number(process.env.PORT),
+    }),
   });
-  let data = response.status;
-  console.log(data);
+  let status = response.status;
+  console.log(status);
 })();
